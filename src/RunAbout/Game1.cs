8083a68ed9985dda09ade13261ffb9e680be7d52
Michael C. Tiberio <github@tiberio.org>
2022-11-10 19:37:12 -0800

    Adding Lulu.

--- src/RunAbout/Game1.cs
+++ src/RunAbout/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -7,7 +8,7 @@ namespace RunAbout;
 public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
-    private SpriteBatch? _spriteBatch;
+    [AllowNull] private SpriteBatch _spriteBatch;
 
     public Game1()
     {
@@ -23,11 +24,15 @@ public class Game1 : Game
         base.Initialize();
     }
 
+    [AllowNull] Texture2D Lulu;
+
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+
+        Lulu = Content.Load<Texture2D>(@"png\lulu");
     }
 
     protected override void Update(GameTime gameTime)
@@ -45,6 +50,10 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(Lulu, new Rectangle(0, 0, 50, 100), Color.White);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
